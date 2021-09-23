3f7b68d6c180069b4fde7d0c8ea0cd357b427e3f
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-23 18:21:03 +0200

    create a route for the view of the app

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express()
 
 const port = 3000
 
+// definition de la route principale, va servir de point d'acces a l'application
+// la route sera la racine de l'app : /
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
 // Demarrage du server nodejs js via l'app express sur un port particulier
 // le port à une valeur par default en cas de non configuration
 app.listen(port || 3000, () => {
