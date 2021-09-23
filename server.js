a9b059121f0065e74704f8d0214946bc7356c3fe
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-23 19:09:35 +0200

    render a file and add some jsdoc comment

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const port = 3000
 
-// definition de la route principale, va servir de point d'acces a l'application
-// la route sera la racine de l'app : /
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.use(express.static(path.join(__dirname, 'public')))
+
+/**
+ * Express router to mount user related functions on.
+ * @type {object}
+ * @const
+ * @namespace GameRoutes
+ */
+const router = express.Router()
+
+/**
+ * Rends la vue de l'application sur l'url root.
+ * @name get/
+ * @function
+ * @memberof module:routers/~GameRoutes
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ */
+router.get('/', (req, res) => {
+  res.sendFile('/index.html')
 })
 
 // Demarrage du server nodejs js via l'app express sur un port particulier
