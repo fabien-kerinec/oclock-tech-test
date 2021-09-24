6c7f257788a8fdde5980a0b98b8440771cdb5950
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-24 21:55:56 +0200

    start define the game

--- src/services/Game.js
+++ src/services/Game.js
@@ -1,3 +1,13 @@
+const { uuid } = require('uuidv4')
+const Board = require('./Board')
 class Game {
-  constructor() {}
+  constructor() {
+    // Crée un id unique pour la game
+    this.id = uuid()
+    // Definis la durée max de la partie
+    // la valeur est definie par default dans le env mais prend une valeur par default de 5 minute en ms
+    this.gameDuration = config.GAME_DURATION || 60 * 5 * 1000
+    // Crée une instance de board, cela representera le plateau
+    this.Board = new Board()
+  }
 }
