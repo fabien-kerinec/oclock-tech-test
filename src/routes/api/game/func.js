d14f4dcf0b873b06a209f18127f3750131b15463
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-25 12:08:58 +0200

    instanciate a new game on the main route api

--- src/routes/api/game/func.js
+++ src/routes/api/game/func.js
@@ -1,8 +1,11 @@
+const Game = require('../../../services/Game')
+
 let model = {}
 model.collection = {}
 model.resource = {}
 
 model.resource.create = (req, res, next) => {
+  const GameInstance = new Game()
   return res.json({ response: 'test' })
 }
 
