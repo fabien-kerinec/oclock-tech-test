9fa21312ba1c11678bfbf61d32e2f8ba94989d61
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-25 22:08:51 +0200

    add mongoose and add env / config variable

--- config.js
+++ config.js
@@ -5,6 +5,10 @@ require('dotenv').config()
  * @prop {string} APP_PORT port utilisé par l'application
  * @prop {number} GAME_DURATION le temps max d'une game
  * @prop {number} GAME_PAIR le nombre de pair souahité dans le jeu
+ * @prop {number} MONGO_USER l'utilisateur de la base de donnée mongo
+ * @prop {number} MONGO_PASS le mot de passe de votre base de donnée
+ * @prop {number} MONGO_HOST l'url de vote base de donnée
+ * @prop {number} MONGO_DATABASE_NAME le nom de votre database
  */
 
 /**
