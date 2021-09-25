d47bd088e229df281062f340fcc8bb9e7d5d75f2
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-25 12:06:20 +0200

    change uuid lib to use the correct one and add export

--- src/services/Game.js
+++ src/services/Game.js
@@ -1,9 +1,9 @@
-const { uuid } = require('uuidv4')
+const { v4: uuidv4 } = require('uuid')
 const Board = require('./Board')
 class Game {
   constructor() {
     // Crée un id unique pour la game
-    this.id = uuid()
+    this.id = uuidv4()
     // Definis la durée max de la partie
     // la valeur est definie par default dans le env mais prend une valeur par default de 5 minute en ms
     this.gameDuration = config.GAME_DURATION || 60 * 5 * 1000
@@ -11,3 +11,5 @@ class Game {
     this.Board = new Board()
   }
 }
+
+module.exports = Game
