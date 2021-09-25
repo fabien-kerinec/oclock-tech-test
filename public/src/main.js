782489f731ef7d54b9c9abe16ee2493f5f42466f
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-25 19:21:09 +0200

    add remove event listener for the end of the game and when card is valid

--- public/src/main.js
+++ public/src/main.js
@@ -2,68 +2,78 @@ window.onload = function () {
   init()
 }
 
-function turnCard(id) {
-  fetch('/api/game/reveal', {
-    method: 'post',
-    headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ card: id, game: sessionStorage.getItem('game') }),
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res)
-      if (!res.cards.end) {
-        if (res.cards.validArray.length > 0) {
-          res.cards.validArray.map((card) => {
-            const elm = document.querySelector(`.item[data-id="${card.id}"]`)
-            if (!elm.classList.contains('valid')) {
-              document
-                .querySelector(`.item[data-id="${card.id}"] .front`)
-                .classList.add(`card_${card.card}`)
-              elm.classList.add('valid')
-            }
-            if (elm.classList.contains('turned')) {
-              elm.classList.remove('turned')
-            }
-          })
-        }
-        if (res.cards.oldArray.length > 0) {
-          res.cards.oldArray.map((card) => {
-            const elm = document.querySelector(`.item[data-id="${card.id}"]`)
-            if (
-              elm.classList.contains('turned') &&
-              !elm.classList.contains('valid')
-            ) {
-              elm.classList.remove('turned')
-              setTimeout(() => {
+function turnCard() {
+  const id = this.dataset.id
+  const element = document.querySelector(`.item[data-id="${id}"]`)
+  if (
+    !element.classList.contains('turned') &&
+    !element.classList.contains('valid')
+  ) {
+    fetch('/api/game/reveal', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ card: id, game: sessionStorage.getItem('game') }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        if (!res.cards.end) {
+          if (res.cards.validArray.length > 0) {
+            res.cards.validArray.map((card) => {
+              const elm = document.querySelector(`.item[data-id="${card.id}"]`)
+              if (!elm.classList.contains('valid')) {
+                elm.removeEventListener('click', turnCard)
                 document
                   .querySelector(`.item[data-id="${card.id}"] .front`)
-                  .classList.remove(`card_${card.card}`)
-              }, 300)
-            }
-          })
+                  .classList.add(`card_${card.card}`)
+                elm.classList.add('valid')
+              }
+              if (elm.classList.contains('turned')) {
+                elm.classList.remove('turned')
+              }
+            })
+          }
+          if (res.cards.oldArray.length > 0) {
+            res.cards.oldArray.map((card) => {
+              const elm = document.querySelector(`.item[data-id="${card.id}"]`)
+              if (
+                elm.classList.contains('turned') &&
+                !elm.classList.contains('valid')
+              ) {
+                elm.classList.remove('turned')
+                setTimeout(() => {
+                  document
+                    .querySelector(`.item[data-id="${card.id}"] .front`)
+                    .classList.remove(`card_${card.card}`)
+                }, 300)
+              }
+            })
+          }
+          if (res.cards.newArray.length > 0) {
+            res.cards.newArray.map((card) => {
+              const elm = document.querySelector(`.item[data-id="${card.id}"]`)
+              if (
+                !elm.classList.contains('turned') &&
+                !elm.classList.contains('valid')
+              ) {
+                document
+                  .querySelector(`.item[data-id="${card.id}"] .front`)
+                  .classList.add(`card_${card.card}`)
+                elm.classList.add('turned')
+              }
+            })
+          }
+        } else {
+          clearInterval(counter)
+          console.log(res.cards.statusGame)
         }
-        if (res.cards.newArray.length > 0) {
-          res.cards.newArray.map((card) => {
-            const elm = document.querySelector(`.item[data-id="${card.id}"]`)
-            if (
-              !elm.classList.contains('turned') &&
-              !elm.classList.contains('valid')
-            ) {
-              document
-                .querySelector(`.item[data-id="${card.id}"] .front`)
-                .classList.add(`card_${card.card}`)
-              elm.classList.add('turned')
-            }
-          })
-        }
-      } else {
-        clearInterval(counter)
-        console.log(res.cards.statusGame)
-      }
-    })
+      })
+  } else {
+    alert('sorry your game is already finished')
+  }
 }
 
 function init() {
@@ -75,9 +85,7 @@ function init() {
       countdown(res.timer)
       const itemList = document.querySelectorAll('.item')
       itemList.forEach((item) => {
-        item.addEventListener('click', (e) => {
-          turnCard(item.dataset.id)
-        })
+        item.addEventListener('click', turnCard)
       })
     })
 }
@@ -89,8 +97,12 @@ function countdown(countdown) {
     console.log(count)
     if (count <= 0) {
       alert('losse')
+      document.querySelectorAll(`.item`).forEach((item) => {
+        console.log('test')
+        item.removeEventListener('click', turnCard)
+      })
       clearInterval(counter)
     }
-    count = count - 1000
+    count = count - 10000
   }, 1000)
 }
