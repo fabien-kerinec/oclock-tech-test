57a11a3b5481dfac6cc56df4368b662836c55ecf
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-25 12:08:02 +0200

    add pair to config and env

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 /**
  * @typedef EnvironmentConfiguration
  * @prop {string} APP_PORT port utilisé par l'application
- * @prop {string} GAME_DURATION le temps max d'une game
+ * @prop {number} GAME_DURATION le temps max d'une game
+ * @prop {number} GAME_PAIR le nombre de pair souahité dans le jeu
  */
 
 /**
