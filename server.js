ff911ee75966c4b2b34168d6fd943901bb93e5cf
fabien kerinec <fabien.kerinec@gmail.com>
2021-09-25 18:59:01 +0200

    add bodyparser to the app aiming to use post request

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const config = require('./config')
 const routes = require('./src/routes')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // recupere l'ensemble des route (api et view)
 app.use(routes)
